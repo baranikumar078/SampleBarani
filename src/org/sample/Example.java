957cbcae20e533a05270019c89387f52b6747b36
baranikumar078 <baranikumar078@gmail.com>
2021-10-26 23:10:52 +0530

    Pushing Sample

---
+++ src/org/sample/Example.java
@@ -0,0 +1,32 @@
+package org.sample;
+
+public class Example {
+	
+	private void mukesh() {
+		System.out.println("Devops Engineer");
+	}
+
+	private void tamilSelvan() {
+	System.out.println("Senior Devops Engineer");
+	}
+	
+	private void zeal() {
+	System.out.println("Gym master");
+	}
+	
+	private void baraniKumar() {
+	System.out.println("Quality Assurance Testing Engineer");
+	}
+	
+	public static void main(String[] args) {
+		
+		Example e=new Example();
+		e.baraniKumar();
+		e.mukesh();
+		e.tamilSelvan();
+		e.zeal();
+		
+		
+		
+	}
+}
